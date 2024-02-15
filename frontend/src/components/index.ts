5b131d7039cce8798da40ebb04699fd050532521
bishakhne0gi <bneogi102002@gmail.com>
2024-02-15 14:24:45 +0530

    frontend file structuring

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,4 @@
+export {default as Login} from './login/Login';
+export {default as Register} from './register/Register';
+export {default as Navbar} from './navbar/Navbar';
+export {default as Footer} from './footer/Footer';
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./Layout.tsx";
+import { Landing } from "./containers/index.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Landing />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
