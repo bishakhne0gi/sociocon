f818cae02da1a214463665a0066dbc91f721934f
ABHIRUP <abhirupsaha02@gmail.com>
2024-02-18 03:40:51 +0530

    b

--- frontend/src/components/register/Register.tsx
+++ frontend/src/components/register/Register.tsx
@@ -1,12 +1,14 @@
 import { useRegister } from "@/hooks/useAuth";
+import { RegisterFormData } from "@/models/auth.model";
 import { useState } from "react";
 
+
 const Register: React.FC = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-    fullname: "",
-    email: "",
+  const [formData, setFormData] = useState<RegisterFormData>({
+    username: '',
+    password: '',
+    fullname: '',
+    email: '',
     avatar: null,
     coverImage: null,
   });
@@ -15,12 +17,16 @@ const Register: React.FC = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
     try {
       await register(formData);
-    } catch (error) {
+    }
+    catch (error) {
       console.error(error);
     }
   };
+
+
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -30,9 +36,7 @@ const Register: React.FC = () => {
           type='text'
           id='username'
           value={formData.username}
-          onChange={(e) =>
-            setFormData({ ...formData, username: e.target.value })
-          }
+          onChange={(e) => setFormData({ ...formData, username: e.target.value })}
         />
         <label htmlFor='email'>Email</label>
         <input
@@ -46,38 +50,38 @@ const Register: React.FC = () => {
           type='text'
           id='fullname'
           value={formData.fullname}
-          onChange={(e) =>
-            setFormData({ ...formData, fullname: e.target.value })
-          }
+          onChange={(e) => setFormData({ ...formData, fullname: e.target.value })}
         />
         <label htmlFor='avatar'>Avatar</label>
         <input
           type='file'
           id='avatar'
-          value={formData.avatar}
-          onChange={(e) => setFormData({ ...formData, avatar: e.target.value })}
+          accept="image/*"
+          onChange={(e) => {
+            if (!e.target.files) return
+            setFormData({ ...formData, avatar: e.target.files[0] })
+          }}
         />
         <label htmlFor='coverImage'>Cover Image</label>
         <input
           type='file'
           id='coverImage'
-          value={formData.coverImage}
-          onChange={(e) =>
-            setFormData({ ...formData, coverImage: e.target.value })
-          }
+          accept="image/*"
+          onChange={(e) => {
+            if (!e.target.files) return
+            setFormData({ ...formData, coverImage: e.target.files[0] })
+          }}
         />
         <label htmlFor='password'>Password</label>
         <input
           type='password'
           id='password'
           value={formData.password}
-          onChange={(e) =>
-            setFormData({ ...formData, password: e.target.value })
-          }
+          onChange={(e) => setFormData({ ...formData, password: e.target.value })}
         />
         <button type='submit'>Register</button>
-        {error && <p>{error}</p>}
       </form>
+      {error && <p>{error}</p>}
     </>
   );
 };
